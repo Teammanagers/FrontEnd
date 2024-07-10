dea4fd15c8ba999bc7c4a28af99d98e911e3159f
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-07-10 22:43:50 +0900

    style: body에 vw, vh 설정

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,3 @@
-import Modal from '@/components/common/Modal';
-
 export const LoginPage = () => {
-  return (
-    <>
-      Login 페이지입니다.
-      <Modal />
-    </>
-  );
+  return <>Login 페이지입니다.</>;
 };
