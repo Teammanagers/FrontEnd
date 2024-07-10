520473505bf403181ca5b320e766c511de95a38e
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-07-10 20:41:09 +0900

    chore: import 부분 절대 경로로 받아올 수 있게 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@components', replacement: '/src/components' },
+      { find: '@apis', replacement: '/src/apis' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@store', replacement: '/src/store' },
+      { find: '@utils', replacement: '/src/utils' }
+    ]
+  }
+});
