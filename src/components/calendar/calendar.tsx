06164a6af90c345998aa0a255e161de146572e2c
Yean <kjan0929@naver.com>
2024-07-12 18:54:32 +0900

    style: calendar ui 디테일 추가

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -16,6 +16,7 @@ const Calendar = () => {
   return (
     <StyledCalendarWrapper>
       <StyledCalendar
+        locale="en-US"
         value={date}
         onChange={handleDateChange}
         formatDay={(locale, date) => moment(date).format('D')} // MM일 제거 -> 숫자만 보이게
@@ -23,8 +24,8 @@ const Calendar = () => {
         formatMonthYear={(locale, date) => moment(date).format('YYYY. MM')} // 네비게이션에서 2023. 12 이렇게 보이도록 설정
         calendarType="gregory" // 일요일 부터 시작
         showNeighboringMonth={true} // 전달, 다음달 날짜 숨기기
-        next2Label={null} // +1년 & +10년 이동 버튼 숨기기
-        prev2Label={null} // -1년 & -10년 이동 버튼 숨기기
+        next2Label={null} // 년도 이동 버튼 숨기기
+        prev2Label={null} // 년도 이동 버튼 숨기기
         minDetail="year" // 10년단위 년도 숨기기
       />
     </StyledCalendarWrapper>
--- src/components/calendar/styles.tsx
+++ src/components/calendar/styles.tsx
@@ -44,10 +44,12 @@ export const StyledCalendarWrapper = styled.div`
       flex-grow: 0 !important;
       font-size: 24px;
       font-weight: 700;
+      pointer-events: none; // 입력 이벤트 무시
     }
 
     /* 기본 hover 효과 무시 */
-    button:hover,
+    .react-calendar__navigation__prev-button:hover,
+    .react-calendar__navigation__next-button:hover,
     .react-calendar__navigation__label:hover {
       background-color: rgba(255, 255, 255, 1);
     }
@@ -85,10 +87,18 @@ export const StyledCalendarWrapper = styled.div`
       align-items: center;
     }
 
+    /* 요일, 날짜 폰트 */
+    .react-calendar__month-view__weekdays__weekday abbr,
     .react-calendar__month-view__days abbr {
       font-size: 24px;
-      font-weight: 400;
+      font-weight: 500;
     }
+
+    /* 기본 hover 효과 제거 */
+    .react-calendar__tile:hover {
+      background-color: rgba(255, 255, 255, 1);
+    }
+
     /* 오늘 날짜 */
     .react-calendar__tile--now {
       background-color: rgba(255, 255, 255, 1);
@@ -101,6 +111,25 @@ export const StyledCalendarWrapper = styled.div`
       height: 60px;
       border-radius: 50%;
       background-color: rgba(221, 235, 255, 1);
+    }
+
+    /* 선택된 날짜 */
+    .react-calendar__tile--active {
+      background-color: rgba(255, 255, 255, 1);
+    }
+    .react-calendar__tile--active abbr {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      background-color: rgba(92, 158, 255, 1);
+    }
+
+    /* 이전/다음 달 날짜 */
+    .react-calendar__month-view__days__day--neighboringMonth abbr {
+      color: rgba(204, 204, 204, 1);
     }
   }
 `;
