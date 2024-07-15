89517c2acf7766c957fec36941e27ee3c5041e1a
zzzzzuuuuu <agu0830@naver.com>
2024-07-15 23:19:09 +0900

    feat: 아이콘마다 클릭시 navigate 적용

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -8,24 +8,70 @@ import Memo from '@assets/sidebar/memo.svg';
 import File from '@assets/sidebar/file.svg';
 import Team from '@assets/sidebar/team.svg';
 import Mypage from '@assets/sidebar/mypage.svg';
-
+import { useNavigate } from 'react-router-dom';
 export const SideBar = () => {
+  const navigate = useNavigate();
+
   return (
     <SideBarContainer>
       <LogoImg />
       <Hr style={{ margin: '12px 0 18px 0' }} />
       {/*선택되었을 때 / hover 때는 이미지 다르게 */}
-      <HomeLogo />
-      <BellLogo />
-      <ListLogo />
-      <CalendarLogo />
-      <Hr style={{ margin: '6px 0 6px 0' }} />
-      <MemoLogo />
-      <FileLogo />
-      <Hr style={{ margin: '6px 0 6px 0' }} />
-      <TeamLogo />
-      <Hr style={{ margin: '6px 0 6px 0' }} />
-      <MypageLogo />
+      <IconContainer
+        onClick={() => {
+          navigate(`/`);
+        }}
+      >
+        <HomeLogo />
+      </IconContainer>
+      <IconContainer>
+        <BellLogo />
+      </IconContainer>
+      <IconContainer
+        onClick={() => {
+          navigate(`/todo-list`);
+        }}
+      >
+        <ListLogo />
+      </IconContainer>
+      <IconContainer
+        onClick={() => {
+          navigate(`/calendar`);
+        }}
+      >
+        <CalendarLogo />
+      </IconContainer>
+      <Hr />
+      <IconContainer
+        onClick={() => {
+          navigate(`/memo`);
+        }}
+      >
+        <MemoLogo />
+      </IconContainer>
+      <IconContainer
+        onClick={() => {
+          navigate(`/share`);
+        }}
+      >
+        <FileLogo />
+      </IconContainer>
+      <Hr />
+      <IconContainer
+        onClick={() => {
+          navigate(`/management`);
+        }}
+      >
+        <TeamLogo />
+      </IconContainer>
+      <Hr />
+      <IconContainer
+        onClick={() => {
+          navigate(`/mypage`);
+        }}
+      >
+        <MypageLogo />
+      </IconContainer>
     </SideBarContainer>
   );
 };
@@ -36,10 +82,23 @@ const SideBarContainer = styled.div`
   background-color: white;
   display: flex;
   flex-direction: column;
-  //justify-content: center;
   align-items: center;
-  gap: 18px;
+  gap: 6px;
   border: 1px solid red;
+
+  &:hover {
+    width: 158px;
+  }
+`;
+
+const IconContainer = styled.div`
+  width: 100%;
+  height: 49px;
+  background-color: ${(props) => props.theme.colors.background};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `;
 
 const LogoImg = styled(Logo)`
@@ -52,7 +111,7 @@ const Hr = styled.div`
   width: 100%;
   height: 2px;
   background-color: ${(props) => props.theme.colors.subLightBlue};
-  //margin: 18px 0 24px 0;
+  margin: 18px 0 18px 0;
 `;
 
 const HomeLogo = styled(Home)`
