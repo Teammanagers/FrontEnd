35b4a78a49eb604df5011362a893728bd8513b9a
zzzzzuuuuu <agu0830@naver.com>
2024-07-16 02:04:24 +0900

    feat: 사이드바 hover시 텍스트 나타나도록

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,36 @@
 import styled from 'styled-components';
 import Logo from '@assets/sidebar/logo.svg';
 import Home from '@assets/sidebar/home.svg';
+// import HomeClick from '@assets/sidebar/home-click.svg';
 import Bell from '@assets/sidebar/bell.svg';
+// import BellClick from '@assets/sidebar/bell-click.svg';
 import List from '@assets/sidebar/list.svg';
+// import ListClick from '@assets/sidebar/list-click.svg';
 import Calendar from '@assets/sidebar/calendar.svg';
+// import CalendarClick from '@assets/sidebar/calendar-click.svg';
 import Memo from '@assets/sidebar/memo.svg';
+// import MemoClick from '@assets/sidebar/memo-click.svg';
 import File from '@assets/sidebar/file.svg';
+// import FileClick from '@assets/sidebar/file-click.svg';
 import Team from '@assets/sidebar/team.svg';
-import Mypage from '@assets/sidebar/mypage.svg';
+// import TeamClick from '@assets/sidebar/team-click.svg';
+import MyPage from '@assets/sidebar/mypage.svg';
+// import MyPageClick from '@assets/sidebar/mypage-click.svg';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 export const SideBar = () => {
+  const [hover, setHover] = useState<boolean>(false);
   const navigate = useNavigate();
 
   return (
-    <SideBarContainer>
+    <SideBarContainer
+      onMouseEnter={() => {
+        setHover(true);
+      }}
+      onMouseLeave={() => {
+        setHover(false);
+      }}
+    >
       <LogoImg />
       <Hr style={{ margin: '12px 0 18px 0' }} />
       {/*선택되었을 때 / hover 때는 이미지 다르게 */}
@@ -22,24 +39,29 @@ export const SideBar = () => {
           navigate(`/`);
         }}
       >
-        <HomeLogo />
+        {/*<HomeLogo />*/}
+        <Home />
+        {hover && <SideBarText>홈</SideBarText>}
       </IconContainer>
       <IconContainer>
-        <BellLogo />
+        <Bell />
+        {hover && <SideBarText>알림</SideBarText>}
       </IconContainer>
       <IconContainer
         onClick={() => {
           navigate(`/todo-list`);
         }}
       >
-        <ListLogo />
+        <List />
+        {hover && <SideBarText>투두리스트</SideBarText>}
       </IconContainer>
       <IconContainer
         onClick={() => {
           navigate(`/calendar`);
         }}
       >
-        <CalendarLogo />
+        <Calendar />
+        {hover && <SideBarText>캘린더</SideBarText>}
       </IconContainer>
       <Hr />
       <IconContainer
@@ -47,14 +69,16 @@ export const SideBar = () => {
           navigate(`/memo`);
         }}
       >
-        <MemoLogo />
+        <Memo />
+        {hover && <SideBarText>메모</SideBarText>}
       </IconContainer>
       <IconContainer
         onClick={() => {
           navigate(`/share`);
         }}
       >
-        <FileLogo />
+        <File />
+        {hover && <SideBarText>자료실</SideBarText>}
       </IconContainer>
       <Hr />
       <IconContainer
@@ -62,7 +86,8 @@ export const SideBar = () => {
           navigate(`/management`);
         }}
       >
-        <TeamLogo />
+        <Team />
+        {hover && <SideBarText>팀 관리</SideBarText>}
       </IconContainer>
       <Hr />
       <IconContainer
@@ -70,7 +95,8 @@ export const SideBar = () => {
           navigate(`/mypage`);
         }}
       >
-        <MypageLogo />
+        <MyPage />
+        {hover && <SideBarText>마이페이지</SideBarText>}
       </IconContainer>
     </SideBarContainer>
   );
@@ -91,9 +117,19 @@ const SideBarContainer = styled.div`
   }
 `;
 
+const SideBarText = styled.p`
+  font-size: 12px;
+  // active 상태일 때는 700으로
+  font-weight: 400;
+  // active 상태일 때는 black
+  color: ${(props) => props.theme.colors.darkGray};
+  margin-left: 18px;
+`;
+
 const IconContainer = styled.div`
   width: 100%;
   height: 49px;
+  // 전역상태관리로 active 상태를 받아서 active 된 부분은 배경색 및 아이콘 다르게
   background-color: ${(props) => props.theme.colors.background};
   display: flex;
   justify-content: center;
@@ -112,44 +148,4 @@ const Hr = styled.div`
   height: 2px;
   background-color: ${(props) => props.theme.colors.subLightBlue};
   margin: 18px 0 18px 0;
-`;
-
-const HomeLogo = styled(Home)`
-  width: 36px;
-  height: 36px;
-`;
-
-const BellLogo = styled(Bell)`
-  width: 36px;
-  height: 36px;
-`;
-
-const CalendarLogo = styled(Calendar)`
-  width: 36px;
-  height: 36px;
-`;
-
-const ListLogo = styled(List)`
-  width: 36px;
-  height: 36px;
-`;
-
-const MemoLogo = styled(Memo)`
-  width: 36px;
-  height: 36px;
-`;
-
-const FileLogo = styled(File)`
-  width: 36px;
-  height: 36px;
-`;
-
-const TeamLogo = styled(Team)`
-  width: 36px;
-  height: 36px;
-`;
-
-const MypageLogo = styled(Mypage)`
-  width: 36px;
-  height: 36px;
 `;
