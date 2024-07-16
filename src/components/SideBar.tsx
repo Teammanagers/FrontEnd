8c72b5956f3789f453fbd52c053b789e7ab7d72c
zzzzzuuuuu <agu0830@naver.com>
2024-07-16 13:52:18 +0900

    style: 라우팅 path 값에 따라 사이드바의 해당 로고 스타일 다르게

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,26 +1,37 @@
 import styled from 'styled-components';
 import Logo from '@assets/sidebar/logo.svg';
 import Home from '@assets/sidebar/home.svg';
-// import HomeClick from '@assets/sidebar/home-click.svg';
+import HomeClick from '@assets/sidebar/home-click.svg';
 import Bell from '@assets/sidebar/bell.svg';
-// import BellClick from '@assets/sidebar/bell-click.svg';
+import BellClick from '@assets/sidebar/bell-click.svg';
 import List from '@assets/sidebar/list.svg';
-// import ListClick from '@assets/sidebar/list-click.svg';
+import ListClick from '@assets/sidebar/list-click.svg';
 import Calendar from '@assets/sidebar/calendar.svg';
-// import CalendarClick from '@assets/sidebar/calendar-click.svg';
+import CalendarClick from '@assets/sidebar/calendar-click.svg';
 import Memo from '@assets/sidebar/memo.svg';
-// import MemoClick from '@assets/sidebar/memo-click.svg';
+import MemoClick from '@assets/sidebar/memo-click.svg';
 import File from '@assets/sidebar/file.svg';
-// import FileClick from '@assets/sidebar/file-click.svg';
+import FileClick from '@assets/sidebar/file-click.svg';
 import Team from '@assets/sidebar/team.svg';
-// import TeamClick from '@assets/sidebar/team-click.svg';
+import TeamClick from '@assets/sidebar/team-click.svg';
 import MyPage from '@assets/sidebar/mypage.svg';
-// import MyPageClick from '@assets/sidebar/mypage-click.svg';
-import { useNavigate } from 'react-router-dom';
+import MyPageClick from '@assets/sidebar/mypage-click.svg';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 export const SideBar = () => {
   const [hover, setHover] = useState<boolean>(false);
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+  };
+
+  const isActive = (path: string): boolean => {
+    return location.pathname === path;
+  };
+
+  console.log(location);
 
   return (
     <SideBarContainer
@@ -36,67 +47,91 @@ export const SideBar = () => {
       {/*선택되었을 때 / hover 때는 이미지 다르게 */}
       <IconContainer
         onClick={() => {
-          navigate(`/`);
+          handleNavigate(`/`);
         }}
+        selected={isActive(`/`)}
       >
-        {/*<HomeLogo />*/}
-        <Home />
-        {hover && <SideBarText>홈</SideBarText>}
+        {isActive(`/`) ? <HomeClick /> : <Home />}
+        {hover && <SideBarText selected={isActive(`/`)}>홈</SideBarText>}
       </IconContainer>
-      <IconContainer>
-        <Bell />
-        {hover && <SideBarText>알림</SideBarText>}
+      {/*  현재 알림페이지가 없다... 일단 일시적으로 홈으로 라우팅 */}
+      <IconContainer
+        onClick={() => {
+          handleNavigate(`/`);
+        }}
+        selected={isActive(`/`)}
+      >
+        {isActive(`/`) ? <BellClick /> : <Bell />}
+        {hover && <SideBarText selected={isActive(`/`)}>알림</SideBarText>}
       </IconContainer>
       <IconContainer
         onClick={() => {
-          navigate(`/todo-list`);
+          handleNavigate(`/todo-list`);
         }}
+        selected={isActive(`/todo-list`)}
       >
-        <List />
-        {hover && <SideBarText>투두리스트</SideBarText>}
+        {isActive(`/todo-list`) ? <ListClick /> : <List />}
+        {hover && (
+          <SideBarText selected={isActive(`/todo-list`)}>
+            투두리스트
+          </SideBarText>
+        )}
       </IconContainer>
       <IconContainer
         onClick={() => {
-          navigate(`/calendar`);
+          handleNavigate(`/calendar`);
         }}
+        selected={isActive(`/calendar`)}
       >
-        <Calendar />
-        {hover && <SideBarText>캘린더</SideBarText>}
+        {isActive(`/calendar`) ? <CalendarClick /> : <Calendar />}
+        {hover && (
+          <SideBarText selected={isActive(`/calendar`)}>캘린더</SideBarText>
+        )}
       </IconContainer>
       <Hr />
       <IconContainer
         onClick={() => {
           navigate(`/memo`);
         }}
+        selected={isActive(`/memo`)}
       >
-        <Memo />
-        {hover && <SideBarText>메모</SideBarText>}
+        {isActive(`/memo`) ? <MemoClick /> : <Memo />}
+        {hover && <SideBarText selected={isActive(`/memo`)}>메모</SideBarText>}
       </IconContainer>
       <IconContainer
         onClick={() => {
-          navigate(`/share`);
+          handleNavigate(`/share`);
         }}
+        selected={isActive(`/share`)}
       >
-        <File />
-        {hover && <SideBarText>자료실</SideBarText>}
-      </IconContainer>
-      <Hr />
-      <IconContainer
-        onClick={() => {
-          navigate(`/management`);
-        }}
-      >
-        <Team />
-        {hover && <SideBarText>팀 관리</SideBarText>}
+        {isActive(`/share`) ? <FileClick /> : <File />}
+        {hover && (
+          <SideBarText selected={isActive(`/share`)}>자료실</SideBarText>
+        )}
       </IconContainer>
       <Hr />
       <IconContainer
         onClick={() => {
-          navigate(`/mypage`);
+          handleNavigate(`/management`);
         }}
+        selected={isActive(`/management`)}
       >
-        <MyPage />
-        {hover && <SideBarText>마이페이지</SideBarText>}
+        {isActive(`/management`) ? <TeamClick /> : <Team />}
+        {hover && (
+          <SideBarText selected={isActive(`/management`)}>팀 관리</SideBarText>
+        )}
+      </IconContainer>
+      <Hr />
+      <IconContainer
+        onClick={() => {
+          handleNavigate(`/mypage`);
+        }}
+        selected={isActive(`/mypage`)}
+      >
+        {isActive(`/mypage`) ? <MyPageClick /> : <MyPage />}
+        {hover && (
+          <SideBarText selected={isActive(`/mypage`)}>마이페이지</SideBarText>
+        )}
       </IconContainer>
     </SideBarContainer>
   );
@@ -110,27 +145,29 @@ const SideBarContainer = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 6px;
-  border: 1px solid red;
 
   &:hover {
     width: 158px;
   }
 `;
 
-const SideBarText = styled.p`
+interface SelectedProps {
+  selected: boolean;
+}
+
+const SideBarText = styled.p<SelectedProps>`
   font-size: 12px;
-  // active 상태일 때는 700으로
-  font-weight: 400;
-  // active 상태일 때는 black
-  color: ${(props) => props.theme.colors.darkGray};
+  font-weight: ${(isActive) => (isActive ? 700 : 400)};
+  color: ${({ selected, theme }) =>
+    selected ? theme.colors.black : theme.colors.darkGray};
   margin-left: 18px;
 `;
 
-const IconContainer = styled.div`
+const IconContainer = styled.div<SelectedProps>`
   width: 100%;
   height: 49px;
-  // 전역상태관리로 active 상태를 받아서 active 된 부분은 배경색 및 아이콘 다르게
-  background-color: ${(props) => props.theme.colors.background};
+  background-color: ${({ selected, theme }) =>
+    selected ? theme.colors.background : 'white'};
   display: flex;
   justify-content: center;
   align-items: center;
