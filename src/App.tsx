d4eb4103583f269a31c24ac7f3d28bbe7c5e277f
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-07-17 21:45:51 +0900

    style: 랜딩 페이지 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -8,39 +8,40 @@ import { SharePage, ExtraSharePage } from './pages/share';
 import { TodoListPage, ExtraTodoListPage } from './pages/todo-list';
 import ErrorPage from './pages/error';
 import MainPage from './pages/main';
-import { SideBar } from '@components/SideBar.tsx';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <SideBar />
-      <Routes>
-        {/* main page */}
-        <Route path="/" element={<MainPage />} />
-        {/* login page */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/login/:id" element={<ExtraLoginPage />} />
-        {/* calendar page */}
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/calendar/:id" element={<ExtraCalendarPage />} />
-        {/* management page */}
-        <Route path="/management" element={<ManagementPage />} />
-        <Route path="/management/:id" element={<ExtraManagementPage />} />
-        {/* memo page */}
-        <Route path="/memo" element={<MemoPage />} />
-        <Route path="/memo/:id" element={<ExtraMemoPage />} />
-        {/* mypage */}
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/:id" element={<ExtraMyPage />} />
-        {/* share page */}
-        <Route path="/share" element={<SharePage />} />
-        <Route path="/share/:id" element={<ExtraSharePage />} />
-        {/* todolist page */}
-        <Route path="/todo-list" element={<TodoListPage />} />
-        <Route path="/todo-list/:id" element={<ExtraTodoListPage />} />
-        {/* error page */}
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          {/* main page */}
+          <Route path="/" element={<MainPage />} />
+          {/* login page */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login/:id" element={<ExtraLoginPage />} />
+          {/* calendar page */}
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/calendar/:id" element={<ExtraCalendarPage />} />
+          {/* management page */}
+          <Route path="/management" element={<ManagementPage />} />
+          <Route path="/management/:id" element={<ExtraManagementPage />} />
+          {/* memo page */}
+          <Route path="/memo" element={<MemoPage />} />
+          <Route path="/memo/:id" element={<ExtraMemoPage />} />
+          {/* mypage */}
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/:id" element={<ExtraMyPage />} />
+          {/* share page */}
+          <Route path="/share" element={<SharePage />} />
+          <Route path="/share/:id" element={<ExtraSharePage />} />
+          {/* todolist page */}
+          <Route path="/todo-list" element={<TodoListPage />} />
+          <Route path="/todo-list/:id" element={<ExtraTodoListPage />} />
+          {/* error page */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
