42735e36e5626250850eba79769b2a1508fa8f67
Yean <kjan0929@naver.com>
2024-07-22 15:50:50 +0900

    chore: 캘린더 페이지 layout 폴더 생성

--- src/components/calendar/layout/Layout.tsx
+++ src/components/calendar/layout/Layout.tsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Layout = styled.div`
   box-sizing: border-box;
-  /* position: relative; */
   display: flex;
   justify-content: flex-start;
   width: 100vw;
   height: 100vh;
   padding: 100px 0 0 126px;
   background-color: #f9fbff;
-  /* z-index: -999; */
 
   .upcoming-schedule {
     h2 {
@@ -20,4 +18,4 @@ const Container = styled.div`
   }
 `;
 
-export default { Container };
+export default { Layout };
--- src/pages/calendar/calendar.tsx
+++ src/pages/calendar/calendar.tsx
@@ -1,15 +1,15 @@
-import Styled from './styles';
+import Styled from '../../components/calendar/layout/Layout';
 import EventCalendar from '@components/calendar/EventCalendar';
 
 export const CalendarPage = () => {
   return (
     <>
-      <Styled.Container>
+      <Styled.Layout>
         <EventCalendar />
         <div className="upcoming-schedule">
           <h2>다가오는 일정</h2>
         </div>
-      </Styled.Container>
+      </Styled.Layout>
     </>
   );
 };
