4164cf39150c878f32aa1e96c501bbab1abe1ddd
Yean <kjan0929@naver.com>
2024-07-23 15:56:06 +0900

    chore: classnames 설치

--- src/components/todo-list/Todos.tsx
+++ src/components/todo-list/Todos.tsx
@@ -1,68 +1,63 @@
-// import { FC } from 'react';
+import { FC } from 'react';
 import styled from 'styled-components';
 import * as Accordion from '@radix-ui/react-accordion';
-// import classNames from 'classnames';
+import classNames from 'classnames';
 import ChevronDownIcon from '@assets/todo-list/chevron-down.svg';
 
-// interface AccordionTriggerProps
-//   extends React.ComponentPropsWithoutRef<'button'> {
-//   children: React.ReactNode;
-//   className?: string;
-// }
+interface AccordionTriggerProps
+  extends React.ComponentPropsWithoutRef<'button'> {
+  children: React.ReactNode;
+  className?: string;
+}
 
-// interface AccordionContentProps extends React.ComponentPropsWithoutRef<'div'> {
-//   children: React.ReactNode;
-//   className?: string;
-// }
+interface AccordionContentProps extends React.ComponentPropsWithoutRef<'div'> {
+  children: React.ReactNode;
+  className?: string;
+}
 
-// const AccordionTrigger: FC<AccordionTriggerProps> = ({
-//   children,
-//   className,
-//   ...props
-// }) => (
-//   <Accordion.Header className="accordion-header">
-//     <Accordion.Trigger
-//       className={classNames('accordion-trigger', className)}
-//       {...props}
-//     >
-//       {children}
-//       <StyledChevronDownIcon aria-hidden />
-//     </Accordion.Trigger>
-//   </Accordion.Header>
-// );
+const AccordionTrigger: FC<AccordionTriggerProps> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <Accordion.Header className="accordion-header">
+    <Accordion.Trigger
+      className={classNames('accordion-trigger', className)}
+      {...props}
+    >
+      {children}
+      <StyledChevronDownIcon aria-hidden />
+    </Accordion.Trigger>
+  </Accordion.Header>
+);
 
-// const AccordionContent: FC<AccordionContentProps> = ({
-//   children,
-//   className,
-//   ...props
-// }) => (
-//   <Accordion.Content
-//     className={classNames('AccordionContent', className)}
-//     {...props}
-//   >
-//     <div className="AccordionContentText">{children}</div>
-//   </Accordion.Content>
-// );
+const AccordionContent: FC<AccordionContentProps> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <Accordion.Content
+    className={classNames('accordion-content', className)}
+    {...props}
+  >
+    <div className="AccordionContentText">{children}</div>
+  </Accordion.Content>
+);
 
 const Todos = () => {
   return (
     <Container>
       <Accordion.Root type="single" className="accordion-root">
         <Accordion.Item value="item-1" className="accordion-item">
-          <Accordion.Header>
-            <Accordion.Trigger className="accordion-trigger">
-              <strong className="username">이예은</strong>
-              <div className="tag-container">
-                <span className="tag">기획자</span>
-                <span className="tag">프론트엔드</span>
-                <span className="tag">백엔드</span>
-              </div>
-              <StyledChevronDownIcon />
-            </Accordion.Trigger>
-          </Accordion.Header>
-          <Accordion.Content className="accordion-content">
-            {/* 투두리스트 */}
-          </Accordion.Content>
+          <AccordionTrigger>
+            <strong className="username">이예은</strong>
+            <div className="tag-container">
+              <span className="tag">기획자</span>
+              <span className="tag">프론트엔드</span>
+              <span className="tag">백엔드</span>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>투두리스트</AccordionContent>
         </Accordion.Item>
       </Accordion.Root>
     </Container>
