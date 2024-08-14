7ca96059b00d8ebb444e6d35d53fb2e454f4c6d7
김지나 <jina4066@naver.com>
2024-08-14 18:19:55 +0900

    refactor: 댓글, 답글 부분 수정

--- src/components/share/FeedbackSection.tsx
+++ src/components/share/FeedbackSection.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import Feedback from '@assets/mypage/feedback.svg';
+import SelectedFeedback from '@assets/mypage/selected-feedback.svg';
+import Dot from '@assets/mypage/dot.svg';
 import { FileProps } from './FileProps';
+import { Comment } from './Comment';
+import { CommentData } from './CommentData';
+import { TextInput } from './TextInput';
 
 interface FeedbackSectionProps {
   isUploading: boolean;
@@ -14,13 +18,43 @@ export const FeedbackSection: React.FC<FeedbackSectionProps> = ({
   selectedFileId,
   files
 }) => {
+  const [comments, setComments] = useState<CommentData[]>([]);
+  const [feedbackContent, setFeedbackContent] = useState<string>('');
+
+  // 전체 날짜와 시간을 포맷하는 함수
+  const formatDateTime = (date: Date) => {
+    return date.toISOString().slice(0, 16).replace('T', ' ').replace(/-/g, '.');
+  };
+
+  // 날짜만 포맷하는 함수
+  const formatDate = (date: Date) => {
+    return date.toISOString().slice(0, 10).replace(/-/g, '.');
+  };
+
+  const handleFeedbackSubmit = () => {
+    if (feedbackContent.trim() === '') return;
+
+    const newComment: CommentData = {
+      id: Date.now(),
+      author: '사용자',
+      role: '역할',
+      //   date: new Date().toISOString().slice(0, 16).replace('T', ' '),
+      date: formatDateTime(new Date()),
+      content: feedbackContent,
+      replies: []
+    };
+
+    setComments([...comments, newComment]);
+    setFeedbackContent('');
+  };
+
   if (isUploading) {
     return (
       <FeedbackInfoBox>
         <InfoText>자료에 대한 피드백을 남길 수 있습니다. </InfoText>
         <AddFeedbackField>
           <IconContainer>
-            <Feedback />
+            <SelectedFeedback />
           </IconContainer>
           <AddText>피드백</AddText>
         </AddFeedbackField>
@@ -40,11 +74,50 @@ export const FeedbackSection: React.FC<FeedbackSectionProps> = ({
     return (
       <SelectedField>
         <InfoField>
-          <TextField>
-            <div className="fileName">{selectedFile?.name}</div>
-          </TextField>
+          <TitleField>{selectedFile?.name}</TitleField>
+          <Properties>
+            {selectedFile?.size} <Dot />{' '}
+            {selectedFile?.date ? formatDate(new Date(selectedFile.date)) : ''}
+          </Properties>
+          <TagAuthor>{selectedFile?.author}</TagAuthor>
         </InfoField>
-        <WriteField></WriteField>
+        <WriteField>
+          <TextInput
+            placeholder="피드백 내용을 입력해주세요"
+            value={feedbackContent}
+            onChange={(e) => setFeedbackContent(e.target.value)}
+            onSubmit={handleFeedbackSubmit}
+          />
+        </WriteField>
+        <CommentsSection>
+          {comments.map((comment) => (
+            <Comment
+              key={comment.id}
+              comment={comment}
+              onReplySubmit={(commentId, replyContent) => {
+                const updatedComments = comments.map((c) =>
+                  c.id === commentId
+                    ? {
+                        ...c,
+                        replies: [
+                          ...c.replies,
+                          {
+                            id: Date.now(),
+                            author: '사용자',
+                            role: '역할',
+                            date: formatDateTime(new Date()),
+                            content: replyContent,
+                            replies: []
+                          }
+                        ]
+                      }
+                    : c
+                );
+                setComments(updatedComments);
+              }}
+            />
+          ))}
+        </CommentsSection>
       </SelectedField>
     );
   }
@@ -93,6 +166,7 @@ const FeedbackEmptyState = styled.div`
 `;
 
 const SelectedField = styled.div`
+  box-sizing: border-box;
   background-color: white;
   width: 535px;
   height: 631px;
@@ -108,12 +182,45 @@ const InfoField = styled.div`
   width: 486px;
   height: 37px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.silver};
-`;
-
-const TextField = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 10px;
+  gap: 19px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const TitleField = styled.div`
+  height: 27px;
+  font-size: 18px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+const Properties = styled.div`
+  height: 15px;
+  display: flex;
+  flex-direction: row;
+  gap: 7px;
+  font-size: 10px;
+  font-weight: 400;
+  align-items: center;
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.darkGray};
+`;
+
+const TagAuthor = styled.div`
+  width: 38px;
+  height: 24px;
+  border-radius: 3px;
+  padding: 8px 6px;
+  background-color: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.mainBlue};
+  font-size: 9px;
+  font-weight: 500;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const WriteField = styled.div`
@@ -121,6 +228,7 @@ const WriteField = styled.div`
   height: 76px;
   display: flex;
   flex-direction: row;
+  align-items: center;
 `;
 
 const AddText = styled.div`
@@ -140,4 +248,15 @@ const IconContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const CommentsSection = styled.div`
+  width: 486px;
+  padding: 13px 0px;
+  max-height: 400px;
+  overflow-y: auto;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
