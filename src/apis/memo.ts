2be5945dfc132a50c3e7fb6e927edf36675fed0a
zzzzzuuuuu <agu0830@naver.com>
2024-08-16 22:58:21 +0900

    feat: 메모 생성 API 연결

--- src/apis/memo.ts
+++ src/apis/memo.ts
@@ -6,3 +6,23 @@ export const getMemos = async (teamId: number): Promise<MemoResponse> => {
   const response = await Axios.get<MemoResponse>(`/api/team/${teamId}/memo`);
   return response.data;
 };
+
+// 메모 생성
+export const createMemo = async (
+  teamId: number,
+  title: string,
+  tagList: string[],
+  content: string
+) => {
+  try {
+    const response = await Axios.post(`/api/team/${teamId}/memo`, {
+      title: title,
+      tagList: tagList,
+      content: content
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
