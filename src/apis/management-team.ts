364c3f2accafd52fbe5a74e1b63242bf388e4b02
zzzzzuuuuu <agu0830@naver.com>
2024-08-19 00:09:30 +0900

    feat: 팀 멤버 조회 API 연결

--- src/apis/management-team.ts
+++ src/apis/management-team.ts
@@ -1,14 +1,25 @@
 // 팀 조회
 import { Axios } from '@apis/Axios.ts';
 import { TeamData } from '../types/team.ts';
+import { MemberTypes } from '../types/member.ts';
 
 export const getTeamData = async (teamId: number): Promise<TeamData> => {
   try {
     const response = await Axios.get(`/api/team/${teamId}`);
-    console.log(response.data.result.team);
     return response.data.result.team;
   } catch (error) {
     console.log(error);
+    throw error;
+  }
+};
+
+// 팀 멤버 조회
+export const getMembers = async (teamId: number): Promise<MemberTypes> => {
+  try {
+    const response = await Axios.get(`/api/team/${teamId}/member`);
+    return response.data.result.teamMember;
+  } catch (error) {
+    console.error(error);
     throw error;
   }
 };
