b5137240887b1b57ceb9b657813cc387500e0ebc
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-08-23 00:02:54 +0900

    feat: 기존 로그인 확인 구현

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const Axios = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    Authorization: `Bearer ` + (localStorage.getItem('accessToken') || '')
+    Authorization: `Bearer ` + localStorage.getItem('accessToken')
   }
 });
