95f7017472ef9e98921febb6506dc4a9078a0d74
Yean <kjan0929@naver.com>
2024-08-23 04:38:03 +0900

    chore: axios instance 파일 통일

--- src/apis/todo-list.ts
+++ src/apis/todo-list.ts
@@ -1,41 +1,41 @@
-import { instance } from './instance';
+import { Axios } from './axios';
 
 export const getMyTodos = async () => {
-  const response = await instance.get(`/api/member/todo`);
+  const response = await Axios.get(`/api/member/todo`);
   return response;
 };
 
 export const getTeamTodos = async (teamId: number) => {
-  const response = await instance.get(`/api/team/${teamId}/todo`);
+  const response = await Axios.get(`/api/team/${teamId}/todo`);
   return response;
 };
 
 export const createTodo = async (teamManageId: number, todo: string) => {
-  const response = await instance.post(`/api/team/${teamManageId}/todo`, {
+  const response = await Axios.post(`/api/team/${teamManageId}/todo`, {
     title: todo
   });
   return response;
 };
 
 export const updateTodo = async (todoId: number, todo: string) => {
-  const response = await instance.patch(`/api/todo/${todoId}`, {
+  const response = await Axios.patch(`/api/todo/${todoId}`, {
     title: todo
   });
   return response;
 };
 
 export const setTodoCheck = async (todoId: number) => {
-  const response = await instance.patch(`/api/todo/${todoId}/state`);
+  const response = await Axios.patch(`/api/todo/${todoId}/state`);
   return response;
 };
 
 export const deleteTodo = async (todoId: number) => {
-  const response = await instance.delete(`/api/todo/${todoId}`);
+  const response = await Axios.delete(`/api/todo/${todoId}`);
   return response;
 };
 
 export const sendAwakeAlarm = async (teamId: number, todoId: number) => {
-  const response = await instance.post(`/api/team/${teamId}/alarm/${todoId}`, {
+  const response = await Axios.post(`/api/team/${teamId}/alarm/${todoId}`, {
     alarmType: 'TODO_AWAKE'
   });
   return response;
