b0f3ae7fa72f99c2f78caa2db35b45ddb1f609db
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-08-23 13:22:58 +0900

    fix: 이슈 수정

--- src/pages/team/team.tsx
+++ src/pages/team/team.tsx
@@ -25,11 +25,11 @@ export const TeamPage = () => {
     <TeamContainer>
       <TeamIndexContainer>
         {!isLoading &&
-          team.result.teamList.map((item) => {
+          team?.result.teamList.map((item) => {
             return <SearchTeamSection data={item} />;
           })}
         <SelectTeamComponent>
-          {!isLoading && team.result.teamList.length < 5 && (
+          {!isLoading && team?.result.teamList.length < 5 && (
             <>
               <TeamLogoComponent onClick={handleClickCreateButton}>
                 <CreateTeam />
