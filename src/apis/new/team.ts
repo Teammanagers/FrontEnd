2a68b95b71067e8050a171ad60732b748414ae3a
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-11-02 18:53:24 +0900

    fix: team api와 response type이 1:1로 매칭되게 수정

---
+++ src/apis/new/team.ts
@@ -0,0 +1,86 @@
+import { teamApiUrl } from '@/apis/new/urls';
+import { AxiosInstance } from '@/apis/new/axios-instance';
+import {
+  createTeamResponse,
+  deleteTeamResponse,
+  getTeamByCodeResponse,
+  getTeamMemberDetailResponse,
+  getTeamMemberResponse,
+  getTeamResponse
+} from '@/types/new/response/team';
+
+// 팀 생성
+export const createTeam = async (): Promise<createTeamResponse> => {
+  const response = await AxiosInstance.post(teamApiUrl);
+  return response.data;
+};
+
+// 팀 수정
+export const updateTeam = async (teamId: number): Promise<void> => {
+  const response = await AxiosInstance.patch(`${teamApiUrl}/${teamId}`);
+  return response.data;
+};
+
+// 팀 참여
+export const joinTeam = async (teamId: number): Promise<void> => {
+  const response = await AxiosInstance.post(`${teamApiUrl}/${teamId}`);
+  return response.data;
+};
+
+// 팀 비밀번호 생성
+export const createTeamPassword = async (teamId: number): Promise<void> => {
+  const response = await AxiosInstance.patch(
+    `${teamApiUrl}/${teamId}/password`
+  );
+  return response.data;
+};
+
+// 팀 조회
+export const getTeam = async (teamId: number): Promise<getTeamResponse> => {
+  const response = await AxiosInstance.get(`${teamApiUrl}/${teamId}`);
+  return response.data;
+};
+
+// 팀 조회 (팀 코드)
+export const getTeamByCode = async (): Promise<getTeamByCodeResponse> => {
+  const response = await AxiosInstance.get(teamApiUrl);
+  return response.data;
+};
+
+// 팀 멤버 조회
+export const getTeamMember = async (
+  teamId: number
+): Promise<getTeamMemberResponse> => {
+  const response = await AxiosInstance.get(`${teamApiUrl}/${teamId}/member`);
+  return response.data;
+};
+
+// 팀 멤버 조회 (역할 태그 포함)
+export const getTeamMemberDetail = async (
+  teamId: number
+): Promise<getTeamMemberDetailResponse> => {
+  const response = await AxiosInstance.get(
+    `${teamApiUrl}/${teamId}/member/detail`
+  );
+  return response.data;
+};
+
+// 팀 종료
+export const deleteTeam = async (
+  teamId: number
+): Promise<deleteTeamResponse> => {
+  const response = await AxiosInstance.patch(`${teamApiUrl}/${teamId}/state`);
+  return response.data;
+};
+
+// 팀 나가기
+export const exitTeam = async (teamId: number): Promise<void> => {
+  const response = await AxiosInstance.delete(`${teamApiUrl}/${teamId}/exit`);
+  return response.data;
+};
+
+// 팀 멤버 코멘트 생성
+export const createMemberComment = async (): Promise<void> => {
+  const response = await AxiosInstance.post(`${teamApiUrl}/comment`);
+  return response.data;
+};
