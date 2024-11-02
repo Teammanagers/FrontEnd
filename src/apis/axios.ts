9a6c6fb1611f73bc8bbffe6f32e39ddd31d75a5b
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-11-02 18:32:59 +0900

    fix: axios instance 파일명이 라이브러리명과 동일해 이를 다르게 수정

--- src/apis/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const Axios = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    Authorization: `Bearer ` + localStorage.getItem('accessToken')
-  }
-});
