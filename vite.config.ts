84dbb21f05f28fd194675a8f683ebddde1806127
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-11-02 18:31:54 +0900

    docs: 절대 경로 내용 수정 및 type에 대한 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -19,13 +19,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: [
-      { find: '@components', replacement: '/src/components' },
-      { find: '@apis', replacement: '/src/apis' },
-      { find: '@hooks', replacement: '/src/hooks' },
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@store', replacement: '/src/store' },
-      { find: '@utils', replacement: '/src/utils' },
-      { find: '@assets', replacement: '/src/assets' }
+      { find: '@/components', replacement: '/src/components' },
+      { find: '@/apis', replacement: '/src/apis' },
+      { find: '@/hooks', replacement: '/src/hooks' },
+      { find: '@/pages', replacement: '/src/pages' },
+      { find: '@/store', replacement: '/src/store' },
+      { find: '@/utils', replacement: '/src/utils' },
+      { find: '@/assets', replacement: '/src/assets' },
+      { find: '@/types', replacement: '/src/types' }
     ]
   }
 });
