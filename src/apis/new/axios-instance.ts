ec233efb2b114d94556d9042e1672cacd4531b18
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-11-10 16:31:28 +0900

    fix: axios instance 파일의 header 수정

--- src/apis/new/axios-instance.ts
+++ src/apis/new/axios-instance.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 export const AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
   headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
     Authorization: `Bearer ` + localStorage.getItem('accessToken')
   }
 });
